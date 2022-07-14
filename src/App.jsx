1ffe4201df0447b67579f5e336cbec58625c064a
j$m <josemoretti01@gmail.com>
2022-07-14 14:47:15 -0300

    Cart context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import CartContainer from './components/CartContainer';
+import CartProvider from './context/CartContext';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -11,17 +13,28 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<div className="app">
-				<header>
-					<NavBar />
-				</header>
-				<main className="app-main">
-					<Routes>
-						<Route path="/" element={<ItemListContainer containerTitle="Catalogo" />} />
-						<Route path="/categoria/:categoriaId" element={<ItemListContainer />} />
-						<Route path="/detalles/:detalleId" element={<ItemDetailContainer containerTitle="Detalle" />} />
-						<Route path="/cart" element={<CartContainer />} />
-					</Routes>
-				</main>
+				<CartProvider value="test">
+					<header>
+						<NavBar />
+					</header>
+					<main className="app-main">
+						<Routes>
+							<Route
+								path="/"
+								element={<ItemListContainer containerTitle="Catalogo" />}
+							/>
+							<Route
+								path="/categoria/:categoriaId"
+								element={<ItemListContainer />}
+							/>
+							<Route
+								path="/detalles/:detalleId"
+								element={<ItemDetailContainer containerTitle="Detalle" />}
+							/>
+							<Route path="/cart" element={<CartContainer />} />
+						</Routes>
+					</main>
+				</CartProvider>
 				<Footer />
 			</div>
 		</BrowserRouter>
--- src/components/ItemDetailContainer/ItemDetail/index.jsx
+++ src/components/ItemDetailContainer/ItemDetail/index.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useCartContext } from '../../../context/CartContext';
 import ItemCount from './ItemCount';
 import './itemdetail.css';
 
 const ItemDetail = ({ data }) => {
 	const [goToCart, setGoToCart] = useState(false);
+	const { addProduct } = useCartContext();
 
 	const onAdd = (quantity) => {
-		alert(`${quantity} ${quantity > 1 ? 'items' : 'item'} agregado al carrito de compras.`);
+		alert(
+			`${quantity} ${quantity === 1 ? 'item' : 'items'} ${
+				quantity === 1 ? 'agregado' : 'agregados'
+			} al carrito de compras.`
+		);
 		setGoToCart(true);
+		addProduct(data, quantity);
 	};
 
 	return (
@@ -18,9 +25,10 @@ const ItemDetail = ({ data }) => {
 				<h3>{data.name}</h3>
 				<span>{data.price}</span>
 				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore, repudiandae nemo officia
-					explicabo aliquam vel quia sint et quasi? Sapiente velit alias eius perferendis, cum ipsam
-					doloremque? Qui, rerum repellendus..
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore,
+					repudiandae nemo officia explicabo aliquam vel quia sint et quasi?
+					Sapiente velit alias eius perferendis, cum ipsam doloremque? Qui,
+					rerum repellendus..
 				</p>
 				{goToCart ? (
 					<Link to="/cart" className="item-to-cart">
